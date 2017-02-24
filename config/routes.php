608cfcbde5b1c1c57f19a3b3d7ee177c9c67ff41
Chris Nizzardini <chris@asrlodging.com>
2017-02-23 22:45:38 -0800

    code formatting

--- config/routes.php
+++ config/routes.php
@@ -3,10 +3,9 @@ use Cake\Routing\RouteBuilder;
 use Cake\Routing\Router;
 use Cake\Routing\Route\DashedRoute;
 
-Router::plugin(
-    'Yummy',
-    ['path' => '/yummy'],
-    function (RouteBuilder $routes) {
-        $routes->fallbacks(DashedRoute::class);
-    }
-);
+Router::plugin('Yummy', ['path' => '/yummy'], function (RouteBuilder $routes) {
+    
+    $routes->connect('/', ['plugin' => 'Yummy', 'controller' => 'Pages', 'action' => 'index']);
+    
+    $routes->fallbacks(DashedRoute::class);
+});
