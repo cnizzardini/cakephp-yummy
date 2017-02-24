d8227aa5c4aea42fe258e9491fcb925b372b6578
Chris Nizzardini <chris@asrlodging.com>
2017-02-23 23:55:05 -0800

    Prevent display of pages if debug == false

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -3,8 +3,15 @@
 namespace Yummy\Controller;
 
 use App\Controller\AppController as BaseController;
+use Cake\Core\Configure;
+use Cake\Network\Exception\ForbiddenException;
 
 class AppController extends BaseController
 {
-
+    public function beforeFilter(\Cake\Event\Event $event){
+        parent::beforeFilter($event);
+        if( Configure::read('debug') == false ){
+            throw new ForbiddenException('Forbidden');
+        }
+    }
 }
