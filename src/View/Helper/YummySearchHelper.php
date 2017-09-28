40ceb360f52aca06aeb26181643b6ad4c2ac4f86
cnizzardini <cnizzardini@gmail.com>
2017-09-27 18:26:49 -0700

    autoformat

--- src/View/Helper/YummySearchHelper.php
+++ src/View/Helper/YummySearchHelper.php
@@ -1,18 +1,21 @@
 <?php
+
 namespace Yummy\View\Helper;
 
 use Cake\View\Helper;
 
 class YummySearchHelper extends Helper
 {
-    public function basicForm($config=[])
+
+    public function basicForm($config = [])
     {
         $element = 'Yummy.YummySearch/basic-form';
-        
-        if( isset($config['element']) ){
+
+        if (isset($config['element'])) {
             $element = $config['element'];
         }
-        
+
         return $this->_View->element($element, $config);
     }
+
 }
