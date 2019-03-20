0e90c60f733902b25a040a6a0a7f86357499a1d4
cnizzardini <cnizzardini@gmail.com>
2019-03-19 22:54:13 -0400

    Moved finding columns in Schema class

---
+++ src/Service/YummySearch/Schema.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Yummy\Service\YummySearch;
+
+use Cake\Datasource\ConnectionInterface;
+
+class Schema
+{
+    /**
+     * Returns array of columns after checking allow/deny rules
+     *
+     * @param ConnectionInterface $database
+     * @param string $name
+     * @return array
+     *
+     * @throw Yummy\Exception\YummySearch\SchemaException
+     */
+    public function getColumns(ConnectionInterface $database, string $name)
+    {
+        $database->schemaCollection();
+
+        $data = [];
+        $tableName = Inflector::underscore($name);
+
+        $modelName = Inflector::camelize($tableName);
+
+        try{
+            $schema = $this->collection->describe($tableName);
+            $columns = $schema->columns();
+        } catch(\Cake\Database\Exception $e) {
+            throw new Yummy\Exception\YummySearch(
+                "Unable to determine schema. Does this controller have an associated "
+                . "database schema? Try manually defining the model YummySearch should use.  "
+                . "\Cake\Database\Exception: " . $e->getMessage()
+            );
+        }
+
+        foreach ($columns as $column) {
+
+            $allowed = $this->isColumnAllowed($modelName, $column);
+
+            if ($allowed !== false) {
+                continue;
+            }
+
+            $columnMeta = $schema->column($column);
+
+            $data["$modelName.$column"] = [
+                'column' => $column,
+                'text' => Inflector::humanize($column),
+                'type' => $columnMeta['type'],
+                'length' => $columnMeta['length'],
+                'sort-order' => $allowed === true ? null : $allowed
+            ];
+        }
+
+        return $data;
+    }
+}
