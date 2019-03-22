114e3c60508ac365918da98e395c2586ee655698
cnizzardini <cnizzardini@gmail.com>
2019-03-21 21:36:15 -0400

    completed unit tests for Association

--- tests/TestCase/Service/YummySearch/AssociationTest.php
+++ tests/TestCase/Service/YummySearch/AssociationTest.php
@@ -2,13 +2,52 @@
 
 namespace Yummy\Test\TestCase\Service\YummySearch;
 
+use Cake\ORM\TableRegistry;
 use Cake\TestSuite\TestCase;
 use Yummy\Service\YummySearch\Association;
+use Cake\Datasource\ConnectionManager;
 
 class AssociationTest extends TestCase
 {
     public function testGetModels()
     {
-        $this->markTestIncomplete('Not implemented yet.');
+        $query = TableRegistry::get('Teams')->find()->contain([
+            'Divisions' => [
+                'Conferences'
+            ]
+        ]);
+
+        $connection = ConnectionManager::get('test');
+        $association = new Association();
+        $models = $association->getModels($connection, [
+            'query' => $query,
+            'model' => 'Teams',
+        ]);
+
+        $this->assertArrayHasKey('Teams', $models);
+        $this->assertArrayHasKey('Conferences', $models);
+        $this->assertArrayHasKey('Divisions', $models);
+    }
+
+    public function testGetModelsWithDeny()
+    {
+        $query = TableRegistry::get('Teams')->find()->contain([
+            'Divisions' => [
+                'Conferences'
+            ]
+        ]);
+
+        $connection = ConnectionManager::get('test');
+        $association = new Association();
+        $models = $association->getModels($connection, [
+            'query' => $query,
+            'model' => 'Teams',
+            'deny' => [
+                'Conferences' => '*'
+            ],
+            'selectGroups' => false
+        ]);
+
+        $this->assertArrayNotHasKey('Conferences', $models);
     }
 }
