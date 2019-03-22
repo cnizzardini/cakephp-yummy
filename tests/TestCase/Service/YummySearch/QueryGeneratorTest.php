39b6af14246b7433ffa490d843058dc3c4a15916
cnizzardini <cnizzardini@gmail.com>
2019-03-21 23:29:46 -0400

    basic unit tests for QueryGenerator

--- tests/TestCase/Service/YummySearch/QueryGeneratorTest.php
+++ tests/TestCase/Service/YummySearch/QueryGeneratorTest.php
@@ -10,7 +10,7 @@ use Yummy\Service\YummySearch\QueryGenerator;
 
 class QueryGeneratorTest extends TestCase
 {
-    public function testGetWhereSingle()
+    public function testGetWhereBaseModelSingleEqualCondition()
     {
         $teamsTable = TableRegistry::get('Teams');
         $teamsTable->addAssociations([
@@ -38,10 +38,6 @@ class QueryGeneratorTest extends TestCase
             'trim' => true
         ];
 
-        $connection = ConnectionManager::get('test');
-        $association = new Association();
-        $models = $association->getModels($connection, $config);
-
         $queryGenerator = new QueryGenerator($config, $query);
         $query = $queryGenerator->getWhere(
             $query,
@@ -49,5 +45,224 @@ class QueryGeneratorTest extends TestCase
             'eq',
             'NY Giants'
         );
+
+        $results = $query->toArray();
+
+        $this->assertCount(1, $results);
+
+        $row = reset($results);
+
+        $this->assertEquals('NY Giants', $row->name);
+    }
+
+    public function testGetWhereBaseModelSingleNotEqualCondition()
+    {
+        $teamsTable = TableRegistry::get('Teams');
+        $teamsTable->addAssociations([
+            'belongsTo' => [
+                'Divisions'
+            ]
+        ]);
+
+        $divisionsTable = TableRegistry::get('Divisions');
+        $divisionsTable->addAssociations([
+            'belongsTo' => [
+                'Conferences'
+            ]
+        ]);
+
+        $query = TableRegistry::get('Teams')->find()->contain([
+            'Divisions' => [
+                'Conferences'
+            ],
+        ]);
+
+        $config = [
+            'query' => $query,
+            'model' => 'Teams',
+            'trim' => true
+        ];
+
+        $queryGenerator = new QueryGenerator($config, $query);
+        $query = $queryGenerator->getWhere(
+            $query,
+            'Teams.id',
+            'not_eq',
+            '1'
+        );
+
+        $results = $query->toArray();
+
+        $this->assertCount(31, $results);
+    }
+
+    public function testGetWhereBaseModelSingleLikeCondition()
+    {
+        $teamsTable = TableRegistry::get('Teams');
+        $teamsTable->addAssociations([
+            'belongsTo' => [
+                'Divisions'
+            ]
+        ]);
+
+        $divisionsTable = TableRegistry::get('Divisions');
+        $divisionsTable->addAssociations([
+            'belongsTo' => [
+                'Conferences'
+            ]
+        ]);
+
+        $query = TableRegistry::get('Teams')->find()->contain([
+            'Divisions' => [
+                'Conferences'
+            ],
+        ])->orderAsc('Teams.name');
+
+        $config = [
+            'query' => $query,
+            'model' => 'Teams',
+            'trim' => true
+        ];
+
+        $queryGenerator = new QueryGenerator($config, $query);
+        $query = $queryGenerator->getWhere(
+            $query,
+            'Teams.name',
+            'like',
+            'NY'
+        );
+
+        $results = $query->toArray();
+
+        $this->assertCount(2, $results);
+
+        $row = reset($results);
+        $this->assertEquals('NY Giants', $row->name);
+
+        $row = end($results);
+        $this->assertEquals('NY Jets', $row->name);
+    }
+
+    public function testGetWhereBaseModelSingleNotLikeCondition()
+    {
+        $teamsTable = TableRegistry::get('Teams');
+        $teamsTable->addAssociations([
+            'belongsTo' => [
+                'Divisions'
+            ]
+        ]);
+
+        $divisionsTable = TableRegistry::get('Divisions');
+        $divisionsTable->addAssociations([
+            'belongsTo' => [
+                'Conferences'
+            ]
+        ]);
+
+        $query = TableRegistry::get('Teams')->find()->contain([
+            'Divisions' => [
+                'Conferences'
+            ],
+        ])->orderAsc('Teams.name');
+
+        $config = [
+            'query' => $query,
+            'model' => 'Teams',
+            'trim' => true
+        ];
+
+        $queryGenerator = new QueryGenerator($config, $query);
+        $query = $queryGenerator->getWhere(
+            $query,
+            'Teams.name',
+            'not_like',
+            'NY G'
+        );
+
+        $results = $query->toArray();
+
+        $this->assertCount(31, $results);
+    }
+
+    public function testGetWhereBaseModelSingleGreaterThanCondition()
+    {
+        $teamsTable = TableRegistry::get('Teams');
+        $teamsTable->addAssociations([
+            'belongsTo' => [
+                'Divisions'
+            ]
+        ]);
+
+        $divisionsTable = TableRegistry::get('Divisions');
+        $divisionsTable->addAssociations([
+            'belongsTo' => [
+                'Conferences'
+            ]
+        ]);
+
+        $query = TableRegistry::get('Teams')->find()->contain([
+            'Divisions' => [
+                'Conferences'
+            ],
+        ])->orderAsc('Teams.name');
+
+        $config = [
+            'query' => $query,
+            'model' => 'Teams',
+            'trim' => true
+        ];
+
+        $queryGenerator = new QueryGenerator($config, $query);
+        $query = $queryGenerator->getWhere(
+            $query,
+            'Teams.id',
+            'gt',
+            '1'
+        );
+
+        $results = $query->toArray();
+
+        $this->assertCount(31, $results);
+    }
+
+    public function testGetWhereBaseModelSingleLessThanCondition()
+    {
+        $teamsTable = TableRegistry::get('Teams');
+        $teamsTable->addAssociations([
+            'belongsTo' => [
+                'Divisions'
+            ]
+        ]);
+
+        $divisionsTable = TableRegistry::get('Divisions');
+        $divisionsTable->addAssociations([
+            'belongsTo' => [
+                'Conferences'
+            ]
+        ]);
+
+        $query = TableRegistry::get('Teams')->find()->contain([
+            'Divisions' => [
+                'Conferences'
+            ],
+        ])->orderAsc('Teams.name');
+
+        $config = [
+            'query' => $query,
+            'model' => 'Teams',
+            'trim' => true
+        ];
+
+        $queryGenerator = new QueryGenerator($config, $query);
+        $query = $queryGenerator->getWhere(
+            $query,
+            'Teams.id',
+            'lt',
+            '10'
+        );
+
+        $results = $query->toArray();
+
+        $this->assertCount(9, $results);
     }
 }
