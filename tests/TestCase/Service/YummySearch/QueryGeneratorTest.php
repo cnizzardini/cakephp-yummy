384a77caacab14d8755c95e46f18b9595b25666a
cnizzardini <cnizzardini@gmail.com>
2019-03-21 21:56:02 -0400

    comments

--- tests/TestCase/Service/YummySearch/QueryGeneratorTest.php
+++ tests/TestCase/Service/YummySearch/QueryGeneratorTest.php
@@ -3,6 +3,8 @@
 namespace Yummy\Test\TestCase\Service\YummySearch;
 
 use Cake\TestSuite\TestCase;
+use Cake\Datasource\ConnectionManager;
+use Cake\ORM\TableRegistry;
 use Yummy\Service\YummySearch\QueryGenerator;
 
 class QueryGeneratorTest extends TestCase
@@ -10,5 +12,26 @@ class QueryGeneratorTest extends TestCase
     public function testGetWhere()
     {
         $this->markTestIncomplete('Not implemented yet.');
+        /*
+        $query = TableRegistry::get('Teams')->find()->contain([
+            'Divisions' => [
+                'Conferences'
+            ],
+        ]);
+
+        $config = [
+            'query' => $query,
+            'model' => 'Teams',
+            'allow' => [
+                'Teams' => ['name'],
+            ]
+        ];
+
+        $connection = ConnectionManager::get('test');
+        $association = new Association();
+        $models = $association->getModels($connection, $config);
+
+        $queryGenerator = new QueryGenerator($config);
+        */
     }
 }
