83df3ff541ce59e57a26288426d78d2811b4366c
cnizzardini <cnizzardini@gmail.com>
2019-03-21 21:49:44 -0400

    completed Schema testing

--- tests/TestCase/Service/YummySearch/SchemaTest.php
+++ tests/TestCase/Service/YummySearch/SchemaTest.php
@@ -3,12 +3,102 @@
 namespace Yummy\Test\TestCase\Service\YummySearch;
 
 use Cake\TestSuite\TestCase;
+use Cake\Datasource\ConnectionManager;
+use Cake\ORM\TableRegistry;
+use Yummy\Service\YummySearch\Association;
 use Yummy\Service\YummySearch\Schema;
+use Yummy\Service\YummySearch\Rule;
 
 class SchemaTest extends TestCase
 {
     public function testGetColumns()
     {
-        $this->markTestIncomplete('Not implemented yet.');
+        $query = TableRegistry::get('Teams')->find()->contain([
+            'Divisions' => [
+                'Conferences'
+            ]
+        ]);
+
+        $config = [
+            'query' => $query,
+            'model' => 'Teams',
+        ];
+
+        $connection = ConnectionManager::get('test');
+        $association = new Association();
+        $models = $association->getModels($connection, $config);
+
+        $rule = new Rule($config);
+        $schema = new Schema($rule);
+
+        $columns = $schema->getColumns($connection, 'Teams');
+
+        $this->assertArrayHasKey('Teams.id', $columns);
+        $this->assertArrayHasKey('Teams.name', $columns);
+        $this->assertArrayHasKey('Teams.abbreviation', $columns);
+
+        $this->assertEquals('id', $columns['Teams.id']['column']);
+        $this->assertEquals('Id', $columns['Teams.id']['text']);
+        $this->assertEquals('smallinteger', $columns['Teams.id']['type']);
+        $this->assertEquals('2', $columns['Teams.id']['length']);
+        $this->assertEquals('', $columns['Teams.id']['sort-order']);
+    }
+
+    public function testGetColumnsCustomAllow()
+    {
+        $query = TableRegistry::get('Teams')->find()->contain([
+            'Divisions' => [
+                'Conferences'
+            ],
+        ]);
+
+        $config = [
+            'query' => $query,
+            'model' => 'Teams',
+            'allow' => [
+                'Teams' => ['name'],
+            ]
+        ];
+
+        $connection = ConnectionManager::get('test');
+        $association = new Association();
+        $models = $association->getModels($connection, $config);
+
+        $rule = new Rule($config);
+        $schema = new Schema($rule);
+
+        $columns = $schema->getColumns($connection, 'Teams');
+
+        $this->assertArrayHasKey('Teams.name', $columns);
+        $this->assertArrayNotHasKey('Teams.id', $columns);
+    }
+
+    public function testGetColumnsCustomDeny()
+    {
+        $query = TableRegistry::get('Teams')->find()->contain([
+            'Divisions' => [
+                'Conferences'
+            ],
+        ]);
+
+        $config = [
+            'query' => $query,
+            'model' => 'Teams',
+            'deny' => [
+                'Teams' => ['id'],
+            ]
+        ];
+
+        $connection = ConnectionManager::get('test');
+        $association = new Association();
+        $models = $association->getModels($connection, $config);
+
+        $rule = new Rule($config);
+        $schema = new Schema($rule);
+
+        $columns = $schema->getColumns($connection, 'Teams');
+
+        $this->assertArrayHasKey('Teams.name', $columns);
+        $this->assertArrayNotHasKey('Teams.id', $columns);
     }
 }
