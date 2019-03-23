2f4f5747e83ff852829bd6316d7470c8d7a912b9
cnizzardini <cnizzardini@gmail.com>
2019-03-22 22:08:35 -0400

    completed testGetYummyHelperData

--- tests/TestCase/Service/YummySearch/ViewHelperTest.php
+++ tests/TestCase/Service/YummySearch/ViewHelperTest.php
@@ -3,12 +3,47 @@
 namespace Yummy\Test\TestCase\Service\YummySearch;
 
 use Cake\TestSuite\TestCase;
-
+use Cake\ORM\TableRegistry;
+use Cake\Datasource\ConnectionManager;
+use Cake\Http\ServerRequest;
+use Yummy\Service\YummySearch\ViewHelper;
+use Yummy\Service\YummySearch\Option;
+use Yummy\Service\YummySearch\Association;
 
 class ViewHelperTest extends TestCase
 {
     public function testGetYummyHelperData()
     {
-        $this->markTestIncomplete('Not implemented yet.');
+        $query = TableRegistry::get('Teams')->find()->contain([
+            'Divisions' => [
+                'Conferences'
+            ],
+        ]);
+
+        $config = [
+            'query' => $query,
+            'model' => 'Teams',
+            'allow' => [
+                'Teams' => ['name'],
+            ],
+            'operators' => [
+                'eq' => 'Equals'
+            ],
+            'selectGroups' => true
+        ];
+
+        $option = new Option($config);
+        $viewHelper = new ViewHelper($config, $option);
+        $association = new Association();
+        $request = new ServerRequest();
+
+        $connection = ConnectionManager::get('test');
+        $models = $association->getModels($connection, $config);
+
+        $data = $viewHelper->getYummyHelperData($models, $request);
+
+        $this->assertArrayHasKey('Teams', $data['models']);
+        $this->assertArrayHasKey('Conferences', $data['models']);
+        $this->assertArrayHasKey('Divisions', $data['models']);
     }
 }
