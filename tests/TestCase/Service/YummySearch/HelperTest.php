e4a179ce332bc0abc0636d60ed8b53eeaf0585c4
cnizzardini <cnizzardini@gmail.com>
2019-03-22 20:43:24 -0400

    placeholder tests

---
+++ tests/TestCase/Service/YummySearch/HelperTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Yummy\Test\TestCase\Service\YummySearch;
+
+use Cake\TestSuite\TestCase;
+
+
+class HelperTest extends TestCase
+{
+    public function testGetFormattedData()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    public function testCheckComponents()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+}
