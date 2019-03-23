a7985573a9dcb12702146d1cb270d3f5bf725249
cnizzardini <cnizzardini@gmail.com>
2019-03-23 13:10:31 -0400

    completed full test

--- tests/TestCase/Controller/Component/YummySearchComponentTest.php
+++ tests/TestCase/Controller/Component/YummySearchComponentTest.php
@@ -2,8 +2,11 @@
 
 namespace Yummy\Test\TestCase\Controller\Component;
 
-use Cake\Controller\ComponentRegistry;
 use Cake\TestSuite\TestCase;
+use Cake\Controller\ComponentRegistry;
+use Cake\ORM\TableRegistry;
+use Cake\Http\ServerRequest;
+use Cake\Network\Response;
 use Yummy\Controller\Component\YummySearchComponent;
 
 /**
@@ -11,56 +14,66 @@ use Yummy\Controller\Component\YummySearchComponent;
  */
 class YummySearchComponentTest extends TestCase
 {
-
-    /**
-     * Test subject
-     *
-     * @var \Yummy\Controller\Component\YummySearchComponent
-     */
-    public $YummySearchComponent;
-
-    /**
-     * setUp method
-     *
-     * @return void
-     */
-    public function setUp()
-    {
-        parent::setUp();
-        //$registry = new ComponentRegistry();
-        //$this->YummySearchComponent = new YummySearchComponent($registry);
-        $this->markTestIncomplete('Not implemented yet.');
-    }
-
-    /**
-     * tearDown method
-     *
-     * @return void
-     */
-    public function tearDown()
-    {
-        unset($this->YummySearchComponent);
-
-        parent::tearDown();
-    }
-
-    /**
-     * Test beforeRender method
-     *
-     * @return void
-     */
-    public function testBeforeRender()
-    {
-        $this->markTestIncomplete('Not implemented yet.');
-    }
-
-    /**
-     * Test search method
-     *
-     * @return void
-     */
     public function testSearch()
     {
-        $this->markTestIncomplete('Not implemented yet.');
+        $request = new ServerRequest();
+        $request = $request->withQueryParams([
+            'YummySearch' => [
+                'field' => [
+                    'Teams.name',
+                ],
+                'operator' => [
+                    'eq',
+                ],
+                'search' => [
+                    'NY Giants',
+                ]
+            ]
+        ]);
+        $response = new Response();
+
+        $request->action = 'index';
+
+        $controller = $this->getMockBuilder('Cake\Controller\Controller')
+            ->setConstructorArgs([$request, $response])
+            ->setMethods(null)
+            ->getMock();
+
+        $controller->loadComponent('Paginator');
+
+        $teamsTable = TableRegistry::get('Teams');
+        $teamsTable->addAssociations([
+            'belongsTo' => [
+                'Divisions'
+            ]
+        ]);
+
+        $divisionsTable = TableRegistry::get('Divisions');
+        $divisionsTable->addAssociations([
+            'belongsTo' => [
+                'Conferences'
+            ]
+        ]);
+
+        $query = TableRegistry::get('Teams')->find()->contain([
+            'Divisions' => [
+                'Conferences'
+            ],
+        ]);
+
+        $registry = new ComponentRegistry($controller);
+        $yummySearch = new YummySearchComponent($registry, [
+            'query' => $query,
+            'model' => 'Teams',
+            'trim' => true
+        ]);
+
+        $query = $yummySearch->search($query);
+
+        $results = $query->toArray();
+
+        $row = reset($results);
+
+        $this->assertEquals(21, $row->id);
     }
 }
