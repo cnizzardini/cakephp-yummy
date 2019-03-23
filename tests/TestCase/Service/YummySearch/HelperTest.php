d972024702ca1693cf1a93f1218fd16b5bcac937
cnizzardini <cnizzardini@gmail.com>
2019-03-22 21:09:30 -0400

    completed test for testGetFormattedData

--- tests/TestCase/Service/YummySearch/HelperTest.php
+++ tests/TestCase/Service/YummySearch/HelperTest.php
@@ -3,13 +3,41 @@
 namespace Yummy\Test\TestCase\Service\YummySearch;
 
 use Cake\TestSuite\TestCase;
+use Cake\Http\ServerRequest;
+use Yummy\Service\YummySearch\Helper;
 
 
 class HelperTest extends TestCase
 {
     public function testGetFormattedData()
     {
-        $this->markTestIncomplete('Not implemented yet.');
+        $request = new ServerRequest();
+        $request = $request->withQueryParams([
+            'YummySearch' => [
+               'field' => [
+                   'Conferences.name',
+                   //'Divisions.name',
+               ],
+               'operator' => [
+                   'eq',
+                   //'like'
+               ],
+               'search' => [
+                   'NFC',
+                   //'East'
+               ]
+            ]
+        ]);
+
+        $data = Helper::getFormattedData($request);
+
+        $this->assertArrayHasKey('field', $data[0]);
+        $this->assertArrayHasKey('operator', $data[0]);
+        $this->assertArrayHasKey('search', $data[0]);
+
+        $this->assertEquals('Conferences.name',$data[0]['field']);
+        $this->assertEquals('eq',$data[0]['operator']);
+        $this->assertEquals('NFC',$data[0]['search']);
     }
 
     public function testCheckComponents()
