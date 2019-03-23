203e51f4cbe176aa43a4c3ce2982cfbff09dbd2f
cnizzardini <cnizzardini@gmail.com>
2019-03-22 20:29:37 -0400

    Moved Helper methods into their own class

--- src/Controller/Component/YummySearchComponent.php
+++ src/Controller/Component/YummySearchComponent.php
@@ -9,6 +9,7 @@ use Yummy\Service\YummySearch\QueryGenerator;
 use Yummy\Service\YummySearch\Rule;
 use Yummy\Service\YummySearch\Association;
 use Yummy\Service\YummySearch\Option;
+use Yummy\Service\YummySearch\Helper;
 
 /**
  * This component is a should be used in conjunction with the YummySearchHelper for building rudimentary search filters
@@ -69,8 +70,10 @@ class YummySearchComponent extends Component
         // check components
         $this->checkComponents();
 
-        // set array for use by YummySearchHelper
-        $yummy = $this->getYummyHelperData();
+        $option = new Option($this->_config);
+        $helper = new Helper($this->_config, $option);
+
+        $yummy = $helper->getYummyHelperData($this->models, $this->controller->request);
 
         // make yummy search data available to view
         $this->controller->set('YummySearch', $yummy);
@@ -128,129 +131,6 @@ class YummySearchComponent extends Component
                 __('YummySearch requires Paginator Component')
             );
         }
-    }
-
-    /**
-     * Retrieves an array used by YummySearchHelper
-     * @return array
-     */
-    private function getYummyHelperData()
-    {
-        $selectOptions = [];
-
-        foreach ($this->models as $camelName => $model) {
-            $selectOptions = array_merge(
-                $selectOptions,
-                $this->getYummyMetaColumns($model, $camelName)
-            );
-        }
-
-        if ($this->getConfig('selectGroups') === false) {
-            $select = [];
-            foreach ($selectOptions as $options) {
-                $select = array_merge($select, $options);
-            }
-
-            // apply A-Z sort when select groups are not used
-            usort($select, function($a, $b) {
-                return strcmp($a['text'], $b['text']);
-            });
-        }
-
-        $yummy = [
-            'base_url' => $this->controller->request->here,
-            'rows' => $this->controller->request->query('YummySearch'),
-            'operators' => $this->config('operators'),
-            'models' => isset($select) ? $select : $selectOptions
-        ];
-
-        return $yummy;
-    }
-
-    /**
-     * Retrieves columns to build dropdowns
-     *
-     * @param string $model
-     * @param string $camelName
-     * @return array
-     */
-    private function getYummyMetaColumns(array $model, string $camelName)
-    {
-        $selectOptions = [];
-
-        $request = $this->controller->request;
-
-        foreach ($model['columns'] as $column => $field) {
-
-            $humanName = $model['humanName'];
-
-            $meta = $this->getYummyMeta($camelName, $field['column']);
-
-            $element = [
-                'text' => ($meta['niceName'] !== false) ? $meta['niceName'] : $field['text'],
-                'path' => $model['path'],
-                'value' => $column,
-                'data-items' => ($meta['options'] !== false) ? implode(',', $meta['options']) : false,
-                'data-type' => ($meta['options'] !== false) ? 'list' : $field['type'],
-                'data-length' => $field['length'],
-                'selected' => ($request->query('YummySearch') === null && $meta['default'] === true) ? true : false
-            ];
-
-            if ($field['sort-order'] === false) {
-                $selectOptions[$humanName][] = $element;
-                continue;
-            }
-
-            $key = $field['sort-order'];
-            if ($key !== null && !isset($selectOptions[$humanName][$key])) {
-                $selectOptions[$humanName][$key] = $element;
-            } else {
-                $selectOptions[$humanName][] = $element;
-            }
-        }
-
-        return $selectOptions;
-    }
-
-    /**
-     * returns yummy meta data for a column
-     * @param mixed $element
-     * @return array
-     */
-    private function getColumnYummyMeta($element) : array
-    {
-        $option = new Option($this->_config);
-
-        return [
-            'niceName' => $option->getColumnNiceName($element),
-            'options' => $option->getColumnOptions($element),
-            'default' => $option->getColumnDefault($element),
-        ];
-    }
-
-    /**
-     * returns yummy meta data for a column
-     * @param string $model
-     * @param string $column
-     * @return array
-     */
-    private function getYummyMeta($model, $column)
-    {
-        $meta = [
-            'options' => false,
-            'niceName' => false,
-            'default' => false,
-        ];
-
-        $config = $this->_config;
-
-        if (isset($config['allow'][$model][$column])) {
-            $meta = $this->getColumnYummyMeta($config['allow'][$model][$column]);
-        } elseif (isset($config['allow'][$model]['_columns'][$column])) {
-            $meta = $this->getColumnYummyMeta($config['allow'][$model]['_columns'][$column]);
-        }
-
-        return $meta;
     }
 
     /**
